a2f83c28540f18c3b4d24e9299b3ff2f0165e976
BuBuHuy <luuvanhuy2012@gmail.com>
2019-11-21 16:52:34 +0700

    ....

--- src/reducers/trendinglaptop.js
+++ src/reducers/trendinglaptop.js
@@ -8,13 +8,12 @@ var initialState = [
         "image": "https://cdn.tgdd.vn/Products/Images/44/145919/acer-spin-3-sp314-51-39wk-i3-7130u-nxguwsv001-cava-1-600x600.jpg",
         "description": "/Man hinh: 14 inch, Full HD/CPU: Core i3 Kabylake, 2.70 GHz/RAM: 4 GB, HDD: 500 GB, Ho tro khe cam SSD M.2 PCIe/Do hoa: Intel HD Graphics 620/HDH: Windows 10 Home SL/Nang: 1.7 kg, Pin: 3 cell",
         "trending": 1,
-    },];
+    },
+];
 var appReducer = (state = initialState, action) => {
     switch (action.type) {
         case types.FETCH_TRENDING_LAPTOPS:
-            console.log('action',action)
             state = action.trendinglaptop;
-            console.log("state",state);
             return [...state];
         default: return state;
     }
